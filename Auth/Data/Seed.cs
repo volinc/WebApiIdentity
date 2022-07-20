6d0d989e6b94e0bb9141f5e015c4c716ab0c2ab5
Ruslan Tatarinov <vol.inc@gmail.com>
2022-07-21 01:38:04 +0300

    Update Seed.cs

--- Auth/Data/Seed.cs
+++ Auth/Data/Seed.cs
@@ -2,7 +2,7 @@
 
 public static class Seed
 {
-    public static readonly DateTimeOffset CreatedAt = DateTimeOffset.UtcNow;
+    public static readonly DateTimeOffset CreatedAt = new(2022, 1, 1, 0, 0, 0, TimeSpan.Zero);
     public const long AdminUserId = 1;
     public const long AdminRoleId = 1;
     public const long CustomerRoleId = 2;
